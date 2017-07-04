223f5ea34990d2e4365e6175f2f82ce6f35369f5
Jesse Hill <jsnahill@gmail.com>
2017-07-04 17:20:41 -0500

    Create first component

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        Hello I am working!
+      </div>
+    );
+  }
+}
+
+export default App;
