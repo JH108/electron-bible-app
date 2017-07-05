11f9f97c5cbe01f374e8ad1b61a1f42244573191
Jesse Hill <jsnahill@gmail.com>
2017-07-05 17:48:17 -0500

    Build components to be used throughout app

---
+++ src/reuseable-components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="footer">
+    <h1 className="start-reading-now">START READING NOW</h1>
+  </div>
+);
+
+export default Footer;
---
+++ src/reuseable-components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MenuButton from './MenuButton';
+
+const Header = () => (
+  <div className="header">
+    <MenuButton />
+    <h1 className="esv-bible">
+    ESV Bible</h1>
+  </div>
+);
+
+export default Header;
